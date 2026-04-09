76e010de72e414ee5422f1032840be330324f172
iamavijerry <iamavijerry@users.noreply.github.com>
2026-04-10 01:49:03 +0530

    print corresponding day name

---
+++ file9.cpp
@@ -0,0 +1,37 @@
+// Take a day number (1–7) and print the corresponding day name.
+
+#include<iostream>
+using namespace std;
+
+void days(int n){
+
+  switch (n){
+    case 1:
+    cout << "Monday" <<endl ;
+    break;
+    case 2:
+    cout << "Tuesday" <<endl ;
+    break;
+    case 3:
+    cout << "Wednesday" <<endl ;
+    break;
+    case 4:
+    cout << "Thursday" <<endl ;
+    break;
+    case 5:
+    cout << "Friday" <<endl ;
+    break;
+    case 6:
+    cout << "Saturday" <<endl;
+    break;
+    case 7:
+    cout << "Sunday" <<endl ;
+    break;
+    default:
+    cout << "Invalid Case." ;
+  }
+} 
+
+int main() { 
+  days(6);
+}
